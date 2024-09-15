8dee5d925694fdb51a3964fb9ba39d13be826514
Anmol Budhiraja <anmolbudhiraja1999@gmail.com>
2024-09-15 15:12:49 -0700

    Add Starter Code

---
+++ src/Series.java
@@ -0,0 +1,46 @@
+public class Series {
+    
+    // Instance Variables
+    private String[] rowNames;
+    private int[] data;
+
+    public Series(String[] _rowNames, int[] _data) {
+        // TODO
+    }
+
+    public String toString() {
+         // TODO
+    }
+
+    public int getLength() {
+         // TODO
+    }
+
+    public String[] getRowNames() {
+         // TODO
+    }
+
+    public String[] getData() {
+        // TODO
+    }
+
+    public void append(String rn, int d) {
+         // TODO
+    }
+
+    public int loc(String rn) {
+        // TODO
+    }
+
+    public int[] loc(String[] rn) {
+        // TODO
+    }
+
+    public int iloc(int ind) {
+         // TODO
+    }
+
+    public boolean drop(String d) {
+         // TODO
+    }
+}
